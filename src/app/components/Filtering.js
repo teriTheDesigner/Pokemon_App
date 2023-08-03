a31e7bc6f5a87f9ce35c79e464b67bce6d9c8ba1
teriTheDesigner <tere1109@stud.kea.dk>
2023-08-03 21:01:56 +0200

    little styling on filters

--- src/app/components/Filtering.js
+++ src/app/components/Filtering.js
@@ -14,29 +14,127 @@ export default function Filtering() {
 
   return (
     <aside className="   sticky  w-40 h-screen top-0 left-0 border-r-2  ">
-      <div className=" sticky w-full h-1/2  top-20 flex flex-col items-end pr-4">
-        <button onClick={() => filterPokemons("favorites")}>
+      <div className=" text-xs leading-5 sticky w-full h-1/2  top-20 flex flex-col items-end pr-8 text-slate-400 ">
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("favorites")}
+        >
           My Favorites ❤️
         </button>
-        <button onClick={() => filterPokemons("All")}>All Types</button>
-        <button onClick={() => filterPokemons("bug")}>Bug</button>
-        <button onClick={() => filterPokemons("dark")}>Dark</button>
-        <button onClick={() => filterPokemons("dragon")}>Dragon</button>
-        <button onClick={() => filterPokemons("ghost")}>Ghost</button>
-        <button onClick={() => filterPokemons("grass")}>Grass</button>
-        <button onClick={() => filterPokemons("ground")}>Ground</button>
-        <button onClick={() => filterPokemons("ice")}>Ice</button>
-        <button onClick={() => filterPokemons("electric")}>Electric</button>
-        <button onClick={() => filterPokemons("fairy")}>Fairy</button>
-        <button onClick={() => filterPokemons("fighting")}>Fighting</button>
-        <button onClick={() => filterPokemons("flying")}>Flying</button>
-        <button onClick={() => filterPokemons("normal")}>Normal</button>
-        <button onClick={() => filterPokemons("poison")}>Poison</button>
-        <button onClick={() => filterPokemons("psychic")}>Psychic</button>
-        <button onClick={() => filterPokemons("rock")}>Rock</button>
-        <button onClick={() => filterPokemons("steel")}>Steel</button>
-        <button onClick={() => filterPokemons("fire")}>Fire</button>
-        <button onClick={() => filterPokemons("water")}>Water</button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("All")}
+        >
+          All Types
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("bug")}
+        >
+          Bug
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("dark")}
+        >
+          Dark
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("dragon")}
+        >
+          Dragon
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("ghost")}
+        >
+          Ghost
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("grass")}
+        >
+          Grass
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("ground")}
+        >
+          Ground
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("ice")}
+        >
+          Ice
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("electric")}
+        >
+          Electric
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("fairy")}
+        >
+          Fairy
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("fighting")}
+        >
+          Fighting
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("flying")}
+        >
+          Flying
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("normal")}
+        >
+          Normal
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("poison")}
+        >
+          Poison
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("psychic")}
+        >
+          Psychic
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("rock")}
+        >
+          Rock
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("steel")}
+        >
+          Steel
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("fire")}
+        >
+          Fire
+        </button>
+        <button
+          className="hover:text-slate-100"
+          onClick={() => filterPokemons("water")}
+        >
+          Water
+        </button>
       </div>
     </aside>
   );
